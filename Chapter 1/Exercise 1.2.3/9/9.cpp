d46edf322a2b0bc206a5a47f53b4cc28ac9d2e7f
kantuni <henrikh.kantuni@gmail.com>
2017-05-03 09:14:56 -0400

    Exercise 1.2.3 - C++ solution

---
+++ Chapter 1/Exercise 1.2.3/9/9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string convert(string n, int from, int to) {
+  return "";
+}
+
+int main() {
+  int x = 16, y1 = 10, y2 = 2;
+  string xn = "FF";
+  cout << convert(xn, x, y1) << "\n";
+  cout << convert(xn, x, y2) << "\n";
+  return 0;
+}
