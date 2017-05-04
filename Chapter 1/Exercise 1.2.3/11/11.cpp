f439b504d5c883b77240a0cc504284a354cc98f5
kantuni <henrikh.kantuni@gmail.com>
2017-05-04 08:33:32 -0400

    Exercise 1.2.3 - 11 C++

---
+++ Chapter 1/Exercise 1.2.3/11/11.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string evaluate(string expression) {
+  // evaluate ()
+  // evaluate *
+  // evaluate /
+  // evaluate +
+  // evaluate -
+  
+  return expression;
+}
+
+int main() {
+  string expression;
+  cin >> expression;
+  cout << evaluate(expression) << "\n";  
+  return 0;
+}
