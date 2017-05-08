19d1d9284498f5aaef2218697496b8dbf849d359
kantuni <henrikh.kantuni@gmail.com>
2017-05-08 07:47:34 -0400

    Exercise 1.3.2 - 1 (C++)

---
+++ Chapter 1/Exercise 1.3.2/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int T;
+  cin >> T;
+  for (int t = 0; t < T; ++t) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << "\n";
+  }
+  return 0;
+}
--- Chapter 1/Exercise 1.3.2/Exercise 1.3.2.md
+++ Chapter 1/Exercise 1.3.2/Exercise 1.3.2.md
@@ -1,1 +1,7 @@
 ### Exercise 1.3.2*
+
+```shell
+g++ -std=c++14 1.cpp
+./a.out
+```
+
