dc1f6f4c71d3e27701b2bec9bb9b9d4d2f9759e5
kantuni <henrikh.kantuni@gmail.com>
2017-05-08 09:35:52 -0400

    Exercise 1.3.2 - 1 - Java

--- Chapter 1/Exercise 1.3.2/1.java
+++ Chapter 1/Exercise 1.3.2/1.java
@@ -13,15 +13,28 @@ class Main {
     }
     
     String next() {
-      while (tokenizer == null || tokenizer.hasMoreElements()) {
+      while (tokenizer == null || !tokenizer.hasMoreElements()) {
         try {
+          tokenizer = new StringTokenizer(reader.readLine());
         } catch (IOException e) {
+          e.printStackTrace();
         }
       }
+      return tokenizer.nextToken();
+    }
+    
+    int nextInt() {
+      return Integer.parseInt(next());
     }
   }
-  
+
   public static void main(String[] args) {
-    
+    FastReader reader = new FastReader();
+    int T = reader.nextInt();
+    for (int t = 0; t < T; ++t) {
+      int a = reader.nextInt();
+      int b = reader.nextInt();
+      System.out.println(a + b);
+    }
   }
 }
