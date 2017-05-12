bc42790525182823924ae360d2ff8d13685ac4da
kantuni <henrikh.kantuni@gmail.com>
2017-05-12 08:02:03 -0400

    Exercise 1.3.2 - 5 - Java

---
+++ Chapter 1/Exercise 1.3.2/5.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main {
+  static class FastReader {
+    BufferedReader reader;
+    StringTokenizer tokenizer;
+    
+    FastReader() {
+      reader = new BufferedReader(new InputStreamReader(System.in));
+      tokenizer = new StringTokenizer("");
+    }
+    
+    boolean hasNext() throws IOException {
+      String line = reader.readLine();
+      if (line != null) {
+        tokenizer = new StringTokenizer(line);
+        return true;
+      }
+      return false;
+    }
+    
+    String next() throws IOException {
+      while (!tokenizer.hasMoreElements()) {
+        tokenizer = new StringTokenizer(reader.readLine());
+      }
+      return tokenizer.nextToken();
+    }
+    
+    int nextInt() throws IOException {
+      return Integer.parseInt(next());
+    }
+  }
+
+  public static void main(String[] args) {
+    FastReader reader = new FastReader();
+    try {
+      int t = 1;
+      while (reader.hasNext()) {
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        if (t > 1) {
+          System.out.println();
+        }
+        System.out.println("Case " + t + ": " + (a + b));
+        ++t;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
