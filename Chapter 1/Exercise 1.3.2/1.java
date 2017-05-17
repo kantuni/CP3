2064c4794e8b89bf63237a8886827abf9289c176
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2017-05-16 20:59:47 -0400

    Update 1.java

--- Chapter 1/Exercise 1.3.2/1.java
+++ Chapter 1/Exercise 1.3.2/1.java
@@ -10,31 +10,32 @@ class Main {
     
     FastReader() {
       reader = new BufferedReader(new InputStreamReader(System.in));
+      tokenizer = new StringTokenizer("");
     }
     
-    String next() {
-      while (tokenizer == null || !tokenizer.hasMoreElements()) {
-        try {
-          tokenizer = new StringTokenizer(reader.readLine());
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
+    String next() throws IOException {
+      while (!tokenizer.hasMoreElements()) {
+        tokenizer = new StringTokenizer(reader.readLine());
       }
       return tokenizer.nextToken();
     }
     
-    int nextInt() {
+    int nextInt() throws IOException {
       return Integer.parseInt(next());
     }
   }
 
   public static void main(String[] args) {
     FastReader reader = new FastReader();
-    int T = reader.nextInt();
-    for (int t = 0; t < T; ++t) {
-      int a = reader.nextInt();
-      int b = reader.nextInt();
-      System.out.println(a + b);
+    try {
+      int T = reader.nextInt();
+      for (int t = 0; t < T; ++t) {
+        int a = reader.nextInt();
+        int b = reader.nextInt();
+        System.out.println(a + b);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 }
